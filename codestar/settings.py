a955351952a40b05a0ff25aa7f49ca500bc964e2
Mikej87 <michaeljib87@gmail.com>
2026-03-10 14:06:49 +0000

    Fix database URL parsing

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
 import os
 
 import dj_database_url
@@ -91,7 +92,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(
+        os.environ.get(
+            "DATABASE_URL",
+            "postgres://localhost:5432/codestar_db"
+        )
+    )
 }
 
 
