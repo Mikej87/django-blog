0dda9fbd2da4165e7fb63902c1e9843d21daa6f9
Mikej87 <michaeljib87@gmail.com>
2026-03-12 21:53:20 +0000

    updated database

--- codestar/settings.py
+++ codestar/settings.py
@@ -96,12 +96,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 # }
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
-    )
-}
+
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
