43688db063674a8557596f57ae987bc7887637b9
Mikej87 <michaeljib87@gmail.com>
2026-03-12 23:41:01 +0000

    updated views.py

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,9 @@ from django.views import generic
 from .models import Post
 # Create your views here.
 
-
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
-    template_name = "blog/index.html"
-    context_object_name = "post_list"
-    paginate_by =6
-
-    
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    context_object_name = "post_list"  # <--- ADD THIS LINE
+    paginate_by = 6
