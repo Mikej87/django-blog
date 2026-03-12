aeca09a8a4b83841e4556214b14193323b0633e1
Mikej87 <michaeljib87@gmail.com>
2026-03-12 21:12:37 +0000

    added import env to settings.py

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,7 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
+    context_object_name = "post_list"
     paginate_by =6
+
+    
