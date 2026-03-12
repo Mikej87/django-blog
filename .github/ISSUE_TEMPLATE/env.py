9d598579c588a2280a2eba1df3e16edee9c9faa1
Mikej87 <michaeljib87@gmail.com>
2026-03-12 22:10:05 +0000

    changed env.py

--- .github/ISSUE_TEMPLATE/env.py
+++ .github/ISSUE_TEMPLATE/env.py
@@ -1,10 +1,8 @@
 import os
 
-os.environ.setdefault(
-    "DATABASE_URL",
-    "postgresql://neondb_owner:npg_6eHQlv8zTPXq@"
-    "ep-mute-shape-agu27ndc.c-2.eu-central-1.aws.neon.tech/"
-    "scan_card_silk_790535")
+os.environ.setdefault("DATABASE_URL", 
+"postgres://u809kcl8m7mmfu:p29e152d2437f9995777dffba050e3ab5dc6e94e7a9a63fa8185ab3ea3d84db1" \
+"0@://cbhnv71uilek74.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com")
 os.environ.setdefault(
     "SECRET_KEY",
     'django-insecure@Tjgdy87@87@#%$^&*()_+|}{":?><,./;\'[]\\=-`~',
