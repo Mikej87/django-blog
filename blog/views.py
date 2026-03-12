6dc41964e9b11daa4124055c5a873ebbae443b9f
Mikej87 <michaeljib87@gmail.com>
2026-03-12 23:55:55 +0000

    Ensure template path is correct

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,6 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    template_name = "blog/index.html"
     context_object_name = "post_list"  
     paginate_by = 6
