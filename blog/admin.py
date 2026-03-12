7b74cc32b0c335d86f87680ff2f555cd9c23e9e0
Mikej87 <michaeljib87@gmail.com>
2026-03-12 12:49:38 +0000

    Changed url path and installed summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 # Register your models here.
-admin.site.register(Post)
 
 
-@admin.register(Comment)
+admin.site.register(Comment)
+
+
 class CommentAdmin(admin.ModelAdmin):
     # This creates the table columns you see in the list
     list_display = ('author', 'body', 'post', 'created_on', 'approved')
--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),
+    path('admin/', admin.site.urls), # Add this line back in!
+    path('blog/', include('blog.urls')),
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('blog.urls'), name='home'),
 ]
