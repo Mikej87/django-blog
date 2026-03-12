86065e0cf7a15fe4465929820c21516d8bd0a7eb
Mikej87 <michaeljib87@gmail.com>
2026-03-12 23:46:56 +0000

    updated index

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
-    context_object_name = "post_list"  # <--- ADD THIS LINE
+    context_object_name = "post_list"  
     paginate_by = 6
