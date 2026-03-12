9fd0f4e9cc27464af41e42e42e73e9353c8c30bd
Mikej87 <michaeljib87@gmail.com>
2026-03-12 22:55:38 +0000

    updated env.py

--- env.py
+++ env.py
@@ -1,10 +1,7 @@
 import os
 
-os.environ.setdefault("DATABASE_URL", "postgres://u809kcl8m7mmfu:p29e152d2437" \
-"f9995777dffba050e3ab5dc6e94e7a9a63fa8185ab3ea3d84db10@cbhnv71uilek74.cluster-c" \
-"zz5s0kz4scl.eu-west-1.rds.amazonaws.com/d8nf982i8nquve")
-os.environ.setdefault(
-    "SECRET_KEY",
-    'django-insecure@Tjgdy87@87@#%$^&*()_+|}{":?><,./;\'[]\\=-`~',
-    "django-insecure-9l8n5s0#-4+%2b1%2b3%2b5%2b6%2b7%2b8%2b9%2b0%2b1%2b2%2b3"
-    "%2b4%2b5%2b6%2b7%2b8%2b9")
+os.environ.setdefault("DATABASE_URL", "postgres://u809kcl8m7mmfu:p" \
+"29e152d2437f9995777dffba050e3ab5dc6e94e7a9a63fa8185ab3ea3d84db10@://" \
+"cbhnv71uilek74.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com")
+
+os.environ.setdefault("SECRET_KEY", "your-unique-secret-key-12345")
