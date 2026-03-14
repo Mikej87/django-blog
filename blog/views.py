6e23d7d544c6a96608cb6d1c430cf44bdbcdb9af
Mikej87 <michaeljib87@gmail.com>
2026-03-14 11:30:06 +0000

    Updated filepath for img

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,14 @@ from django.views import generic
 from .models import Post
 # Create your views here.
 
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "blog/index.html"
     context_object_name = "post_list"  
     paginate_by = 6
+
 
 def post_detail(request, slug):
     """
