60365d1e75501e8f25243e7502f318cbaa6f0798
Mikej87 <michaeljib87@gmail.com>
2026-03-17 14:03:06 +0000

    Finished testing

--- env.py
+++ env.py
@@ -1,8 +1,8 @@
 import os
 
 # Notice: No colon at the end, and fixed the @ symbol area
-os.environ.setdefault("DATABASE_URL", "postgres://u809kcl8m7mmfu:p" \
-"29e152d2437f9995777dffba050e3ab5dc6e94e7a9a63fa8185ab3ea3d84db10@cbh" \
+os.environ.setdefault("DATABASE_URL", "postgres://u809kcl8m7mmfu:p"\
+"29e152d2437f9995777dffba050e3ab5dc6e94e7a9a63fa8185ab3ea3d84db10@cbh"\
 "nv71uilek74.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com/d8nf982i8nquve")
 
 os.environ.setdefault("SECRET_KEY", "your-unique-secret-key-12345")
